01cdcd8af20c06cc2776bfeffb20d3b68cdec3fa
kimhongbeom <fkdldj48@naver.com>
2025-03-10 09:00:40 +0900

    1

--- src/main/java/com/example/gradlespringboottest/GradleSpringbootTestApplication.java
+++ src/main/java/com/example/gradlespringboottest/GradleSpringbootTestApplication.java
@@ -2,9 +2,16 @@ package com.example.gradlespringboottest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class GradleSpringbootTestApplication {
+public class GradleSpringbootTestApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GradleSpringbootTestApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GradleSpringbootTestApplication.class, args);
